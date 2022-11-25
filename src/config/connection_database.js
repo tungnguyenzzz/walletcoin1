40efcaa0f59d7c9ca6dc94a5e4bef19f57800e9d
anhtp <anhtpde140084@fpt.edu.vn>
2022-11-25 21:13:18 +0700

    updatee

--- src/config/connection_database.js
+++ src/config/connection_database.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('walletcoin', 'root', null, {
-    host: 'localhost',
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     dialect: 'mysql',
-    logging: false
 })
 
 const connectionDatabase = async () => {
